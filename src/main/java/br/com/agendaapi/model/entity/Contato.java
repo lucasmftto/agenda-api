c8c0d884301e6923ece6cfab66f81e2dec8266b8
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-04-24 16:25:48 -0300

    Iniciado projeto de agenda

---
+++ src/main/java/br/com/agendaapi/model/entity/Contato.java
@@ -0,0 +1,31 @@
+package br.com.agendaapi.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter@Setter
+@NoArgsConstructor
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private Boolean favorito;
+
+}
---
+++ src/main/java/br/com/agendaapi/model/repository/Contatorepository.java
@@ -0,0 +1,9 @@
+package br.com.agendaapi.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.agendaapi.model.entity.Contato;
+
+public interface Contatorepository extends JpaRepository<Contato, Integer> {
+
+}
