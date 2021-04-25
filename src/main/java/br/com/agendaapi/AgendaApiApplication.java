214c3542ae83c8e4f7e394e3d3fbcad9c36a8326
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-04-25 13:00:09 -0300

    criacao de Api de contatos

--- src/main/java/br/com/agendaapi/AgendaApiApplication.java
+++ src/main/java/br/com/agendaapi/AgendaApiApplication.java
@@ -1,27 +1,10 @@
 package br.com.agendaapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import br.com.agendaapi.model.entity.Contato;
-import br.com.agendaapi.model.repository.Contatorepository;
 
 @SpringBootApplication
 public class AgendaApiApplication {
-	
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired Contatorepository repo) {
-		return args -> {
-			Contato c = new Contato();
-			c.setNome("Lucas");
-			c.setEmail("lucas@email.com");
-			c.setFavorito(false);
-			repo.save(c);
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApiApplication.class, args);
---
+++ src/main/java/br/com/agendaapi/api/rest/ContatoController.java
@@ -0,0 +1,56 @@
+package br.com.agendaapi.api.rest;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.agendaapi.model.entity.Contato;
+import br.com.agendaapi.model.repository.ContatoRepository;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/contatos")
+@RequiredArgsConstructor
+public class ContatoController {
+	
+	@Autowired
+	private ContatoRepository repository;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Contato save(@RequestBody Contato contato) {
+		return repository.save(contato);	
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		repository.deleteById(id);	
+	}
+	
+	@GetMapping
+	public List<Contato> list(){
+		return repository.findAll();
+	}
+	
+	@PatchMapping("{id}/favorito")
+	public void favorite( @PathVariable Integer id, @RequestBody Boolean favorito) {
+		Optional<Contato> contato = repository.findById(id);
+		contato.ifPresent( c -> {
+			c.setFavorito(favorito);
+			repository.save(c);
+		});
+	}
+
+}
