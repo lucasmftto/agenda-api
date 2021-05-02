a40c0e66c64184bed29b83323b76f54d5b48ddd8
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-05-02 10:51:49 -0300

    Implementando paginacao

--- src/main/java/br/com/agendaapi/api/rest/ContatoController.java
+++ src/main/java/br/com/agendaapi/api/rest/ContatoController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.Part;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -49,8 +52,13 @@ public class ContatoController {
 	}
 	
 	@GetMapping
-	public List<Contato> list(){
-		return repository.findAll();
+	public Page<Contato> list(
+			@RequestParam(value = "page", defaultValue = "0") Integer pagina,
+			@RequestParam(value = "size", defaultValue = "10") Integer tamanhoPagina
+			){
+		Sort sort = Sort.by(Sort.Direction.ASC, "id");
+		PageRequest pageRequest = PageRequest.of(pagina, tamanhoPagina, sort);
+		return repository.findAll(pageRequest);
 	}
 	
 	@PatchMapping("{id}/favorito")
