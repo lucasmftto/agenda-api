8f0b6ec5c7995619b29fa1a99e7dc69d419fa73d
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-05-01 23:04:55 -0300

    Implementando upload de arquivo

--- src/main/java/br/com/agendaapi/api/rest/ContatoController.java
+++ src/main/java/br/com/agendaapi/api/rest/ContatoController.java
@@ -1,8 +1,13 @@
 package br.com.agendaapi.api.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.Part;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,8 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,6 +61,26 @@ public class ContatoController {
 			c.setFavorito(!favorito);
 			repository.save(c);
 		});
+	}
+	
+	@PutMapping("{id}/foto")
+	public byte[] addPhoto(@PathVariable Integer id, @RequestParam("foto") Part arquivo){
+		
+		Optional<Contato> contato = repository.findById(id);
+		
+		return contato.map( c -> {
+			try {
+				InputStream is = arquivo.getInputStream();
+				byte[] bytes =  new byte[(int) arquivo.getSize()];
+				IOUtils.readFully(is, bytes);
+				c.setFoto(bytes);
+				repository.save(c);
+				is.close();
+				return bytes;
+			} catch (IOException e) {
+				return null;
+			}
+		}).orElse(null);
 	}
 
 }
